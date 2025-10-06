c337669422c28d8e10cefcaab5013ba13827df8f
katruong855 <katruong855@gmail.com>
2025-10-06 03:06:37 -0700

    Connected Health Slider + Pause Screen Effect
    - When importing Game HUD, attach Player prefab to player field in Health/Healthbar script
    
    - Added dark overlay for pause screen, ensure that PauseSystem is the top most gameobject to get that nice overlay

--- Assets/scripts/Healthbar.cs
+++ Assets/scripts/Healthbar.cs
@@ -1,20 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
+    
+    public GameObject player;
+    public Slider healthSlider;
+    Health playerhealth;
 
-    //PlayerController playerscript = GameObject.Find("player").GetComponent<PlayerController>();
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        //Debug.Log(player == null);
+        playerhealth = player.GetComponent<Health>();
+        // Debug.Log("health:");
+        // Debug.Log(playerhealth == null);
+        // Debug.Log(healthSlider.maxValue);
+
+        healthSlider.wholeNumbers = true; //Accept whole numbers only
+        healthSlider.minValue = 0; //Set min and max health
+        healthSlider.maxValue = playerhealth.GetMaxHealth();
+        healthSlider.value = playerhealth.GetHealth(); //Set current value of Healthbar
+
+        //Debug.Log(healthSlider.value);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //Update health on slider
+    private void Update(){
+
+        healthSlider.value = playerhealth.GetHealth(); 
     }
 }
