45c3b1804378f57b9c6c4d26e21b55a36eb6266e
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2025-10-06 11:21:27 -0700

    boss balancing + sprites imported

--- Assets/scripts/Devil_Boss_Fight/Boss Movement.cs
+++ Assets/scripts/Devil_Boss_Fight/Boss Movement.cs
@@ -37,7 +37,7 @@ public class BossMovement : MonoBehaviour
     {
         if (makeDecision) 
         {
-            int choice = Random.Range(0, 4);
+            int choice = Random.Range(0, 5);
             switch (choice) 
             {
                 case 0:
@@ -46,14 +46,15 @@ public class BossMovement : MonoBehaviour
                     break;
                 case 1:
                     if (transform.position.y < heightForHighAttacks) break;
-                    ThreeFlamesAttack();
+                    StartCoroutine(ThreeFlamesAttack(3, .5f));
                     StartCoroutine(WaitForNext(2f));
                     break;
                 case 2:
                     FlamePyre();
                     StartCoroutine(WaitForNext(4f));
                     break;
-                case 3:
+                case 3: //continues to next - 3 and 4 do the same attack
+                case 4:
                     ThrowTrident();
                     StartCoroutine(WaitForNext(3f));
                     break;
@@ -88,15 +89,21 @@ public class BossMovement : MonoBehaviour
         }
     }
 
-    void ThreeFlamesAttack() 
+    IEnumerator ThreeFlamesAttack(int numBursts, float timeBetweenShots) 
     {
-        GameObject a = Instantiate(fireProj, transform.position, Quaternion.identity);
-        GameObject b = Instantiate(fireProj, transform.position, Quaternion.identity);
-        GameObject c = Instantiate(fireProj, transform.position, Quaternion.identity);
+        for (int i = 0; i < numBursts; i++) 
+        {
+            GameObject a = Instantiate(fireProj, transform.position, Quaternion.identity);
+            GameObject b = Instantiate(fireProj, transform.position, Quaternion.identity);
+            GameObject c = Instantiate(fireProj, transform.position, Quaternion.identity);
 
-        a.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Cos(-3 * Mathf.PI / 4), Mathf.Sin(-3 * Mathf.PI / 4)).normalized * projForce, ForceMode2D.Impulse);
-        b.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,-1) * projForce, ForceMode2D.Impulse);
-        c.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Cos(-1 * Mathf.PI / 4), Mathf.Sin(-1 * Mathf.PI / 4)).normalized * projForce, ForceMode2D.Impulse);
+            a.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Cos(-3 * Mathf.PI / 4), Mathf.Sin(-3 * Mathf.PI / 4)).normalized * projForce, ForceMode2D.Impulse);
+            b.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -1) * projForce, ForceMode2D.Impulse);
+            c.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Cos(-1 * Mathf.PI / 4), Mathf.Sin(-1 * Mathf.PI / 4)).normalized * projForce, ForceMode2D.Impulse);
+            yield return new WaitForSeconds(timeBetweenShots);
+        }
+
+        
     }
 
     void FlamePyre() 
@@ -112,12 +119,19 @@ public class BossMovement : MonoBehaviour
 
     void ThrowTrident() 
     {
-        Vector2 randomDir = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)); 
-        randomDir = randomDir + (Vector2)playerObj.transform.position; //roughly towards player with a bit of randomness
+        //Vector2 randomDir = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)); 
+        //randomDir = randomDir + (Vector2)playerObj.transform.position; //roughly towards player with a bit of randomness
+        //GameObject t = Instantiate(trident, transform.position, Quaternion.identity);
+        //t.transform.up = randomDir;
+        //t.GetComponent<Rigidbody2D>().AddForce(randomDir * throwForce, ForceMode2D.Impulse);
+
+        Vector2 target = playerObj.transform.position - transform.position;
+        target += new Vector2(Random.Range(-3,3), Random.Range(-3,3));
         GameObject t = Instantiate(trident, transform.position, Quaternion.identity);
-        t.transform.up = randomDir;
-        t.GetComponent<Rigidbody2D>().AddForce(randomDir * throwForce, ForceMode2D.Impulse);
+        t.transform.up = target;
+        t.GetComponent<Rigidbody2D>().AddForce(target * throwForce, ForceMode2D.Impulse);
     }
+
 
     IEnumerator WaitForNext(float timeToWait) 
     {
